fe09b88a1aac0fd1d4c1c6c2d3414407c84ece61
pswayze-bit <59946063+pswayze-bit@users.noreply.github.com>
2020-09-14 16:31:10 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule} from '@angular/material/toolbar';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -11,12 +10,17 @@ import {RegistrationComponent} from './registration/registration.component';
 import {RegistrationService} from './services/registration.service';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {AppRoutingModule} from './app.routing.module';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import {MatExpansionModule} from '@angular/material/expansion';
 
+const appRoutes: Routes = [
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'register', component: RegistrationComponent},
+  
+];
 
 @NgModule({
   declarations: [
@@ -32,7 +36,7 @@ import {MatExpansionModule} from '@angular/material/expansion';
     MatButtonModule,
     MatInputModule,
     MatCardModule,
-    RouterModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
