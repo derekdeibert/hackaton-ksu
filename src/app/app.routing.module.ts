61dc14c1d62f4ddb035d0e74c0bed3e5b933481f
pswayze-bit <59946063+pswayze-bit@users.noreply.github.com>
2020-09-14 16:32:46 -0400

    Update app.routing.module.ts

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { RegistrationComponent } from './registration/registration.component';
-import {HomepageComponent} from './homepage/homepage.component';
 
-const routes: Routes = [
-  {path: 'homepage', component: HomepageComponent},
-  {path: 'homepage/register', component: RegistrationComponent},
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
