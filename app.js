45def33a65d3fba27775b2de77afd836214351e4
derekdeibert <derekdeibert@gmail.com>
2020-11-06 09:03:27 -0500

    changed port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const express = require('express')
 const indReg = require('./IndRoutes'); //imports routes
 
 const app = express()
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 
 
 app.use(bodyParser.json());
