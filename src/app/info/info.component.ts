d4570642b2ad8cc0a6f453c4542fe888229d5279
derekdeibert <derekdeibert@gmail.com>
2020-11-14 14:35:29 -0500

    updated info component

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,16 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {LoginService} from '../services/login.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
-export class InfoComponent implements OnInit{
-  constructor(public loginService: LoginService) {
-  }
-
-  ngOnInit() {
-    this.loginService.loggedIn = false;
-  }
+export class InfoComponent{
 }
